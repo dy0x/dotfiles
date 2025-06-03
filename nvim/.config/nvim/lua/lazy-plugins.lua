c8dd990da3968b6176b5fb09ed78a25f35634942
dy0x <github@dylanoxenbridge.com>
2025-06-03 12:31:02 +1000

    nvim plugins

---
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -0,0 +1,22 @@
+require('lazy').setup({
+  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+
+  require 'plugins/autopairs',
+  require 'plugins/blink',
+  require 'plugins/conform',
+  require 'plugins/copilot',
+  -- require 'plugins/debug',
+  require 'plugins/gitsigns',
+  require 'plugins/gruvbox',
+  -- require 'plugins/indent_line',
+  require 'plugins/lazydev',
+  -- require 'plugins/lint',
+  require 'plugins/mini',
+  require 'plugins/neo-tree',
+  require 'plugins/telescope',
+  require 'plugins/todo-comments',
+  require 'plugins/treesitter',  
+  require 'plugins/which-key',
+})
+
+-- vim: ts=2 sts=2 sw=2 et
